a940f6942d599b6d98e031c2d5b4dbca8e211768
Defman <jacob@rosborg.dk>
2015-07-20 12:03:27 +0200

    Fuck ij

---
+++ Lostshard/src/main/java/com/lostshard/Alts/IpManager.java
@@ -0,0 +1,20 @@
+package com.lostshard.Alts;
+
+import java.util.List;
+import java.util.UUID;
+
+public class IpManager {
+
+    private static IpManager manager = new IpManager();
+
+    private List<PlayerConnection>
+
+    public static IpManager getManager() {
+        return manager;
+    }
+
+    public UUID[] getFriends(UUID uuid) {
+        return null;
+    }
+
+}
---
+++ Lostshard/src/main/java/com/lostshard/Alts/PlayerConnection.java
@@ -0,0 +1,32 @@
+package com.lostshard.Alts;
+
+import java.net.Inet4Address;
+import java.util.Date;
+import java.util.UUID;
+
+public class PlayerConnection {
+
+    private final Inet4Address ip;
+    private final UUID player;
+    private final Date join;
+    private final Date quit;
+
+    public PlayerConnection(Inet4Address ip, UUID player, Date join, Date quit) {
+        this.ip = ip;
+        this.player = player;
+        this.join = join;
+        this.quit = quit;
+    }
+
+    public PlayerConnection(Inet4Address ip, UUID player) {
+        this(ip, player, new Date(), null);
+    }
+
+    public Inet4Address getIp() {
+        return ip;
+    }
+
+    public UUID getPlayer() {
+        return this.player;
+    }
+}
