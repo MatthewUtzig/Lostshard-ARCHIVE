1742f92753ddc994eaa376c1f8210bc41081e2c2
MatthewUtzig <mu@imi.co>
2018-12-11 09:23:15 -0600

    removed wayback from cheaterlog

--- cheaterlog/index.php
+++ cheaterlog/index.php
@@ -2,22 +2,8 @@
 
 <!DOCTYPE html>
 <html>
-<head><script src="//archive.org/includes/analytics.js?v=cf34f82" type="text/javascript"></script>
-<script type="text/javascript">window.addEventListener('DOMContentLoaded',function(){var v=archive_analytics.values;v.service='wb';v.server_name='wwwb-app104.us.archive.org';v.server_ms=306;archive_analytics.send_pageview({});});</script>
-<script type="text/javascript" src="/static/js/ait-client-rewrite.js?v=1543384230.0" charset="utf-8"></script>
-<script type="text/javascript">
-WB_wombat_Init('https://web.archive.org/web', '20181211145355', 'www.project610.com');
-</script>
-<script type="text/javascript" src="/static/js/wbhack.js?v=1543384230.0" charset="utf-8"></script>
-<script type="text/javascript">
-__wbhack.init('https://web.archive.org/web');
-</script>
-<link rel="stylesheet" type="text/css" href="/static/css/banner-styles.css?v=1543384230.0" />
-<link rel="stylesheet" type="text/css" href="/static/css/iconochive.css?v=1543384230.0" />
-<!-- End Wayback Rewrite JS Include -->
-
-
-	<title>LS Cheater Shit 2.0 - &#9660; Now with sorting! &#9650;</title>
+<head>
+	<title>LS Cheater Log 2.0 - &#9660; Now with sorting! &#9650;</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<meta name="description" content=""/>
 	<meta name="keywords" content=""/>
@@ -73,143 +59,14 @@ __wbhack.init('https://web.archive.org/web');
 
 	<script>
 	function confirmdelete(link) {
-		if (confirm("Are you suuuuuuuuuuuuuuuure?") == true) {
+		if (confirm("Are you suuuuuuuuuuuuuuuure?\nNote: this function has been disabled, but remains here for nostalgia purposes.") == true) {
 			window.location = link;
 		}
 	}
 	</script>
 </head>
 
-
-
-<body><!-- BEGIN WAYBACK TOOLBAR INSERT -->
-<script type="text/javascript" src="/static/js/timestamp.js?v=1543384230.0" charset="utf-8"></script>
-<script type="text/javascript" src="/static/js/graph-calc.js?v=1543384230.0" charset="utf-8"></script>
-<script type="text/javascript" src="/static/js/auto-complete.js?v=1543384230.0" charset="utf-8"></script>
-<script type="text/javascript" src="/static/js/toolbar.js?v=1543384230.0" charset="utf-8"></script>
-<style type="text/css">
-body {
-  margin-top:0 !important;
-  padding-top:0 !important;
-  /*min-width:800px !important;*/
-}
-.wb-autocomplete-suggestions {
-    text-align: left; cursor: default; border: 1px solid #ccc; border-top: 0; background: #fff; box-shadow: -1px 1px 3px rgba(0,0,0,.1);
-    position: absolute; display: none; z-index: 2147483647; max-height: 254px; overflow: hidden; overflow-y: auto; box-sizing: border-box;
-}
-.wb-autocomplete-suggestion { position: relative; padding: 0 .6em; line-height: 23px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; font-size: 1.02em; color: #333; }
-.wb-autocomplete-suggestion b { font-weight: bold; }
-.wb-autocomplete-suggestion.selected { background: #f0f0f0; }
-</style>
-<div id="wm-ipp" lang="en" style="display:none;direction:ltr;">
-<div style="position:fixed;left:0;top:0;right:0;">
-<div id="wm-ipp-inside">
-  <div style="position:relative;">
-    <div id="wm-logo" style="float:left;width:130px;padding-top:10px;">
-      <a href="/web/" title="Wayback Machine home page"><img src="/static/images/toolbar/wayback-toolbar-logo.png" alt="Wayback Machine" width="110" height="39" border="0" /></a>
-    </div>
-    <div class="r" style="float:right;">
-      <div id="wm-btns" style="text-align:right;height:25px;">
-                  <div id="wm-save-snapshot-success">success</div>
-          <div id="wm-save-snapshot-fail">fail</div>
-          <a href="#"
-             onclick="__wm.saveSnapshot('http://www.project610.com/cheatershit/', '20181211145355')"
-             title="Share via My Web Archive"
-             id="wm-save-snapshot-open"
-          >
-            <span class="iconochive-web"></span>
-          </a>
-          <a href="https://archive.org/account/login.php"
-             title="Sign In"
-             id="wm-sign-in"
-          >
-            <span class="iconochive-person"></span>
-          </a>
-          <span id="wm-save-snapshot-in-progress" class="iconochive-web"></span>
-        	<a href="http://faq.web.archive.org/" title="Get some help using the Wayback Machine" style="top:-6px;"><span class="iconochive-question" style="color:rgb(87,186,244);font-size:160%;"></span></a>
-	<a id="wm-tb-close" href="#close" onclick="__wm.h(event);return false;" style="top:-2px;" title="Close the toolbar"><span class="iconochive-remove-circle" style="color:#888888;font-size:240%;"></span></a>
-      </div>
-      <div id="wm-share" style="text-align:right;">
-	<a href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=https://web.archive.org/web/20181211145355/http://www.project610.com/cheatershit/', '', 'height=400,width=600'); return false;" title="Share on Facebook" style="margin-right:5px;" target="_blank"><span class="iconochive-facebook" style="color:#3b5998;font-size:160%;"></span></a>
-	<a href="#" onclick="window.open('https://twitter.com/intent/tweet?text=https://web.archive.org/web/20181211145355/http://www.project610.com/cheatershit/&amp;via=internetarchive', '', 'height=400,width=600'); return false;" title="Share on Twitter" style="margin-right:5px;" target="_blank"><span class="iconochive-twitter" style="color:#1dcaff;font-size:160%;"></span></a>
-      </div>
-    </div>
-    <table class="c" style="">
-      <tbody>
-	<tr>
-	  <td class="u" colspan="2">
-	    <form target="_top" method="get" action="/web/submit" name="wmtb" id="wmtb"><input type="text" name="url" id="wmtbURL" value="http://www.project610.com/cheatershit/" onfocus="this.focus();this.select();" /><input type="hidden" name="type" value="replay" /><input type="hidden" name="date" value="20181211145355" /><input type="submit" value="Go" /></form>
-	  </td>
-	  <td class="n" rowspan="2" style="width:110px;">
-	    <table>
-	      <tbody>
-		<!-- NEXT/PREV MONTH NAV AND MONTH INDICATOR -->
-		<tr class="m">
-		  <td class="b" nowrap="nowrap">Nov</td>
-		  <td class="c" id="displayMonthEl" title="You are here: 14:53:55 Dec 11, 2018">DEC</td>
-		  <td class="f" nowrap="nowrap">Jan</td>
-		</tr>
-		<!-- NEXT/PREV CAPTURE NAV AND DAY OF MONTH INDICATOR -->
-		<tr class="d">
-		  <td class="b" nowrap="nowrap"><img src="/static/images/toolbar/wm_tb_prv_off.png" alt="Previous capture" width="14" height="16" border="0" /></td>
-		  <td class="c" id="displayDayEl" style="width:34px;font-size:24px;white-space:nowrap;" title="You are here: 14:53:55 Dec 11, 2018">11</td>
-		  <td class="f" nowrap="nowrap"><img src="/static/images/toolbar/wm_tb_nxt_off.png" alt="Next capture" width="14" height="16" border="0" /></td>
-		</tr>
-		<!-- NEXT/PREV YEAR NAV AND YEAR INDICATOR -->
-		<tr class="y">
-		  <td class="b" nowrap="nowrap">2017</td>
-		  <td class="c" id="displayYearEl" title="You are here: 14:53:55 Dec 11, 2018">2018</td>
-		  <td class="f" nowrap="nowrap">2019</td>
-		</tr>
-	      </tbody>
-	    </table>
-	  </td>
-	</tr>
-	<tr>
-	  <td class="s">
-	    	    <div id="wm-nav-captures">
-	      	      <a class="t" href="/web/20181211145355*/http://www.project610.com/cheatershit/" title="See a list of every capture for this URL">1 capture</a>
-	      <div class="r" title="Timespan for captures of this URL">11 Dec 2018</div>
-	      </div>
-	  </td>
-	  <td class="k">
-	    <a href="" id="wm-graph-anchor">
-	      <div id="wm-ipp-sparkline" title="Explore captures for this URL" style="position: relative">
-		<canvas id="wm-sparkline-canvas" width="575" height="27" border="0"></canvas>
-	      </div>
-	    </a>
-	  </td>
-	</tr>
-      </tbody>
-    </table>
-    <div style="position:absolute;bottom:0;right:2px;text-align:right;">
-      <a id="wm-expand" class="wm-btn wm-closed" href="#expand" onclick="__wm.ex(event);return false;"><span id="wm-expand-icon" class="iconochive-down-solid"></span> <span style="font-size:80%">About this capture</span></a>
-    </div>
-  </div>
-    <div id="wm-capinfo" style="border-top:1px solid #777;display:none; overflow: hidden">
-            <div style="background-color:#666;color:#fff;font-weight:bold;text-align:center">COLLECTED BY</div>
-    <div style="padding:3px;position:relative" id="wm-collected-by-content">
-      <div style="display:inline-block;vertical-align:top;width:49%;">
-			<span class="c-logo" style="background-image:url(https://archive.org/services/img/liveweb)"></span>
-		<div>Collection: <a style="color:#33f;" href="https://archive.org/details/liveweb" target="_new"><span class="wm-title">Live Web Proxy Crawls</span></a></div>
-		<div style="max-height:75px;overflow:hidden;position:relative;">
-	  <div style="position:absolute;top:0;left:0;width:100%;height:75px;background:linear-gradient(to bottom,rgba(255,255,255,0) 0%,rgba(255,255,255,0) 90%,rgba(255,255,255,255) 100%);"></div>
-	  Content crawled via the <a href="http://archive.org/web/web.php">Wayback Machine</a> Live Proxy mostly by the Save Page Now feature on web.archive.org.
-<br /><br />
-Liveweb proxy is a component of Internet Archive’s wayback machine project. The liveweb proxy captures the content of a web page in real time, archives it into a ARC or WARC file and returns the ARC/WARC record back to the wayback machine to process. The recorded ARC/WARC file becomes part of the wayback machine in due course of time.
-<br /><br />
-	</div>
-	      </div>
-    </div>
-    <div style="background-color:#666;color:#fff;font-weight:bold;text-align:center" title="Timestamps for the elements of this page">TIMESTAMPS</div>
-    <div>
-      <div id="wm-capresources" style="margin:0 5px 5px 5px;max-height:250px;overflow-y:scroll !important"></div>
-      <div id="wm-capresources-loading" style="text-align:left;margin:0 20px 5px 5px;display:none"><img src="/static/images/loading.gif" alt="loading" /></div>
-    </div>
-  </div></div></div></div><script type="text/javascript">
-__wm.bt(575,27,25,2,"web","http://www.project610.com/cheatershit/","2018-12-11",1996);
-</script>
-<!-- END WAYBACK TOOLBAR INSERT -->
+<body>
 
 <div style="width: 1150px; height: 100px; margin-top: 50px; margin-left: auto; margin-right: auto; background: #fff; border-top: 1px solid black; border-left: 1px solid black; border-right: 1px solid black;"><a href="?"><img src="/web/20181211145355im_/http://www.project610.com/cheatershit/banner.png" alt="LS Cheater Shit 2.0 Banner!"/></a></div>
 <div style="width: 1150px; border-left: 1px solid black; border-right: 1px solid black; margin-left: auto; margin-right: auto;">
@@ -37017,23 +36874,4 @@ __wm.bt(575,27,25,2,"web","http://www.project610.com/cheatershit/","2018-12-11",
 	<a id="end"></a>
 </div>
 </body>
-</html><!--
-     FILE ARCHIVED ON 14:53:55 Dec 11, 2018 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 14:54:05 Dec 11, 2018.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
--->
-<!--
-playback timings (ms):
-  LoadShardBlock: 239.452 (3)
-  esindex: 0.006
-  captures_list: 260.933
-  CDXLines.iter: 10.588 (3)
-  PetaboxLoader3.datanode: 229.854 (3)
-  exclusion.robots: 1.484
-  exclusion.robots.policy: 1.468
-  RedisCDXSource: 7.208
-  load_resource: 25.571
--->
+</html>
